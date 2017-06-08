d888bed2a9e810b13b609f25b73a683dd21ec951
zilongshanren <guanghui8827@126.com>
2017-06-08 14:57:43 +0800

    improve culling examples

--- assets/cases/culling/culling.js
+++ assets/cases/culling/culling.js
@@ -7,33 +7,36 @@ cc.Class({
 
     // use this for initialization
     onLoad: function () {
-    	cc.director.setDisplayStats(true);
-    	var g = this.getComponent(cc.Graphics);
-    	if(g) {
-	   		g.lineWidth = 10;
-	        g.fillColor = cc.hexToColor('#ff0000');
-	        
-	        g.moveTo(-20, 0);
-	        g.lineTo(0, -100);
-	        g.lineTo(20, 0);
-	        g.lineTo(0, 100);
-	        g.close();
+        this._isFpsShow = cc.director.isDisplayStats();
+        cc.director.setDisplayStats(true);
+        var g = this.getComponent(cc.Graphics);
+        if(g) {
+            g.lineWidth = 10;
+            g.fillColor = cc.hexToColor('#ff0000');
 
-	        g.stroke();
-	        g.fill();
-    	}
+            g.moveTo(-20, 0);
+            g.lineTo(0, -100);
+            g.lineTo(20, 0);
+            g.lineTo(0, 100);
+            g.close();
+
+            g.stroke();
+            g.fill();
+        }
 
      
     },
 
     spawnGameObject: function (event, data) {
-    	cc.log("data : = " +　data)
-    	var node = this.buttons[data|0];
-    	node.runAction(cc.sequence(cc.moveBy(1, cc.p(600,0)), cc.moveBy(1, cc.p(-600,0))));
+        cc.log("data : = " + data);
+        var node = this.buttons[data|0];
+        node.runAction(cc.sequence(cc.moveBy(1, cc.p(600,0)), cc.moveBy(1, cc.p(-600,0))));
     },
 
     onDestroy: function () {
-    	cc.director.setDisplayStats(false);
+        if (!this._isFpsShow) {
+            cc.director.setDisplayStats(false);
+        }
     }
 
     // called every frame, uncomment this function to activate update callback
